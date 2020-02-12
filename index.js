d01fb61a9d87ca18714493be76c23fd493b151d8
Darius Matthews <50760033+DariusMatthews@users.noreply.github.com>
2020-02-12 14:17:40 -0600

    Fixed input validation

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ also add following text to small elements
 
 var submitButton = $('button');
 var signUp = $(".sign-up");
-var form = $("form");
+var form = $("#form");
 var firstName = $('input[name="first name"]');
 var lastName = $('input[name="last name"]');
 var email = $('input[name="email address"]');
@@ -20,37 +20,73 @@ var errorFN = $("#fname");
 var errorLN = $("#lname");
 var errorEM = $("#mail");
 var errorPW = $("#pword");
+var errors = [];
 var validEmail = /[a-zA-Z]+[1-9]*[@]{1}[a-z]+[.]{1}[a-z]/;
+var validation = false;
 
+function formErrors(e) {
+  e.preventDefault();
 
-form.submit(formErrors);
-
-function formErrors(i) {
-
-  if (firstName.text("")) {
+  //input validation
+  if (firstName[0].value === '') {
     firstName.addClass("error");
     errorFN.text("First Name cannot be empty");
     firstName.attr("placeholder", "");
-  } 
+    errors.push('first name');
+  } else {
+    errorFN.text("");
+    firstName.removeClass("error");
+    firstName.attr("placeholder", "First Name");
+  }
 
-  if (lastName.text("")) {
+  if (lastName[0].value === '') {
     lastName.addClass("error");
     errorLN.text("Last Name cannot be empty");
     lastName.attr("placeholder", "");
+    errors.push('last name');
+  } else {
+    errorLN.text("");
+    lastName.removeClass("error");
+    lastName.attr("placeholder", "Last Name");
   }
 
-  if (!validEmail.test(email)) {
+  if (!validEmail.test(email[0].value)) {
     email.addClass("error");
-    errorEM.text("Looks like this is not an email");
     email.attr("placeholder", "");
+    errorEM.text("Looks like this is not an email");
+    errors.push('email');
+  } else {
+    errorEM.text("");
+    email.removeClass("error");
+    email.attr("placeholder", "Email Address");
   }
-
-  if (password.text("")) {
+  if (password[0].value === '') {
     password.addClass("error");
     errorPW.text("Password cannot be empty");
     password.attr("placeholder", "");
+    errors.push('password');
+  } else {
+    errorPW.text("");
+    password.removeClass("error");
+    password.attr("placeholder", "Password");
   }
 
-  signUp.addClass("extra-card");
-  i.preventDefault();
+  //form validation if no errors
+  if (errors.length === 0) {
+    validation = true;
+    console.log('valid form');
+    form[0].reset();
+  }
+
+  //lengthen card when more than 1 error
+  if (errors.length > 1) {
+    signUp.addClass("extra-card");
+    errors = [];
+  } else {
+    signUp.removeClass("extra-card");
+    errors = [];
+  }
+  
 }
+
+form.submit(formErrors);
